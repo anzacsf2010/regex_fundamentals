51357c5d229725a3df00f7b10251245c932fe6aa
Andy St Fort <andy.st.fort@niceincontact.com>
2023-04-12 12:30:01 -0400

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   hello_world_flags.js

--- hello_world_flags.js
+++ hello_world_flags.js
@@ -3,12 +3,19 @@
 const strText = "This is to say hello to everyone who want to say hi to a cool dude! I am sure many of you from around the world would love it too. There are simple tools to use in my town";
 console.log(strText);
 
+console.log('Using RegExp');
+
 const regexpInstance = new RegExp('to', 'igm');
-const result1 = regexpInstance.exec(strText);
-console.log(regexpInstance);
-console.log(result1);
+let result1 = "";
+while (result1 = regexpInstance.exec(strText)) {
+    console.log(result1);
+}
+
+console.log('--------------');
+console.log('Using Literal');
 
 const regexLiteral = /to/gmi;
-const result2 = regexLiteral.exec(strText);
-console.log(regexLiteral);
-console.log(result2);
+let result2 = "";
+while (result2 = regexLiteral.exec(strText)) {
+    console.log(result2);
+}
